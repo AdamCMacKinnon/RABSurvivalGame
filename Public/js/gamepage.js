3dd43a2995f89241276206169902672a03c1a9f7
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-03-21 19:23:01 -0400

    added iframe, styling, removed past picks logic

--- Public/js/gamepage.js
+++ Public/js/gamepage.js
@@ -18,7 +18,6 @@ auth.onAuthStateChanged(user =>{
 
 // TEAM SELECT FORM TO DATABASE
 db.collection('PRODUCTION');
-//DB collection changes week to week.  This allows for authentication of picks
 const submission = document.getElementById('submission');
 
 submission.addEventListener('submit',(e) => {
@@ -63,38 +62,6 @@ const currentPick = document.getElementById('currentPick');
 
 currentPick.innerHTML = `<h5>Your Current Pick: ${JSON.stringify(currentUserObj.team)}</h5>`
 
-const prevPicks = document.getElementById('prevPicks');
-//list previous picks on screen
-
-// const getPick = async() =>{
-//     let response = await db.collection('PRODUCTION').doc(`${submission.twitter.value} + ${submission.week.value}`).get()
-//     let data = await response
-//     return data
-// }
-// let pick = getPick()
-// console.log(pick)
-// 
-let docRef = db.collection("PRODUCTION").doc("rmntcbaseball");
-console.log(docRef)
-
-docRef.get().then((doc) => {
-    if (doc.exists) {
-        localStorage.setItem('pastPicks', JSON.stringify(doc.data()));
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
-
-const pastPicks = localStorage.getItem('pastPicks');
-const pastPicksObj = JSON.parse(pastPicks)
-console.log(pastPicksObj)
-
-
-prevPicks.innerHTML = `<li>Week ${pastPicksObj.week}: ${pastPicksObj.team}</li>`
 
 
 
